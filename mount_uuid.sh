b7958c11b8fbc945bf8e10109badcd8f2b181f77
Vinoth865 <vinothrvino215@gmail.com>
2026-09-20 09:55:47 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -8,21 +8,23 @@
 
 
 # Display filesystem UUID
+sudo blkid
 
 
 
 
 # Create mount directory
+sudo mkdir -p /mnt/mydisk
 
 
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
+sudo mount -U YOUR_UUID /mnt/mydisk
 
 
 
 
 # Display mounted filesystem
-
-
+df -h /mnt/mydisk
